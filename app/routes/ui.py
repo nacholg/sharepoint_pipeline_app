1bd061dba9c6a31e06a2cf779de13d4a130e979e
Juan Ignacio Lopez Guardone <ignaciolopezg@gmail.com>
2026-03-25 12:19:16 -0300

    correct ui.py

--- app/routes/ui.py
+++ app/routes/ui.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
@@ -8,13 +7,15 @@ templates = Jinja2Templates(directory="app/templates")
 
 @router.get("/")
 def home(request: Request):
-    print("HOME SESSION FULL:", dict(request.session))
-
     user = request.session.get("user")
-
+    print("HOME SESSION FULL:", dict(request.session))
     print("HOME USER:", user)
 
-    return templates.TemplateResponse("index.html", {
-        "request": request,
-        "user": user
-    })
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={
+            "request": request,
+            "user": user,
+        },
+    )
